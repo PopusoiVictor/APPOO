fe250167dbd64a3ecf444d0a148ea4dd21993fce
PopusoiVictor <vicu.popusoi@mail.ru>
2018-02-27 08:47:47 +0200

    bad example

--- Laborator 2/src/lab2.ts
+++ Laborator 2/src/lab2.ts
@@ -1,45 +1,26 @@
-interface ISquare {
-    SquareArea(a: number)
-}
-interface IRectangle {
+interface IPerimeter {
     RectanglePerimeter(a: number, b: number)
-}
-interface ICircle {
-    CircleArea(r: number)
-}
-interface ITriangle {
     TrianglePerimeter(a: number, b: number, c: number)
 }
-class Square implements ISquare {
+interface IArea {
+    SquareArea(a: number)
+    CircleArea(r: number)
+}
+interface IType {
+    TriangleType(a: number, b: number, c: number)
+}
+class FigureArea implements IArea {
     public a: number
-    constructor(a: number) {
+    public r: number
+    constructor(a: number, r: number) {
         this.a = a
+        this.r = r
     }
     SquareArea(a): void {
         let result = a * a
         console.log("-----SQUARE-----")
         console.log("Square sides: a = " + a)
         console.log("Square area: " + result)
-    }
-}
-class Rectangle implements IRectangle {
-    public a: number
-    public b: number
-    constructor(a: number, b: number) {
-        this.a = a
-        this.b = b
-    }
-    RectanglePerimeter(a, b): void {
-        let result = (2 * a) + (2 * b)
-        console.log("-----RECTANGLE-----")
-        console.log("Rectangle sides: a = " + a + ", b = " + b)
-        console.log("Rectangle perimeter: " + result)
-    }
-}
-class Circle implements ICircle {
-    public r: number
-    constructor(r: number) {
-        this.r = r
     }
     CircleArea(r): void {
         let pi = 3.14
@@ -49,14 +30,20 @@ class Circle implements ICircle {
         console.log("Circle area: " + result)
     }
 }
-class Triangle implements ITriangle {
+class FigurePerimeter implements IPerimeter {
     public a: number
     public b: number
     public c: number
     constructor(a: number, b: number, c: number) {
         this.a = a
-        this.b = a
-        this.c = a
+        this.b = b
+        this.c = c
+    }
+    RectanglePerimeter(a, b): void {
+        let result = (2 * a) + (2 * b)
+        console.log("-----RECTANGLE-----")
+        console.log("Rectangle sides: a = " + a + ", b = " + b)
+        console.log("Rectangle perimeter: " + result)
     }
     TrianglePerimeter(a, b, c): void {
         let result = a + b + c
@@ -65,10 +52,15 @@ class Triangle implements ITriangle {
         console.log("Triangle perimeter: " + result)
     }
 }
-class Type extends Triangle {
+class Type implements IType {
     public a: number
     public b: number
     public c: number
+    constructor(a: number, b: number, c: number) {
+        this.a = a
+        this.b = b
+        this.c = c
+    }
     TriangleType(a, b, c): void {
         if (a === b === c) {
             console.log("Triangle is equilateral")
@@ -81,12 +73,11 @@ class Type extends Triangle {
         }
     }
 }
-const square = new Square(5)
-square.SquareArea(5)
-const rectangle = new Rectangle(5, 7)
-rectangle.RectanglePerimeter(5, 7)
-const circle = new Circle(4)
-circle.CircleArea(4)
-const triangle = new Type(5, 6, 7)
-triangle.TrianglePerimeter(5, 6, 7)
-triangle.TriangleType(5, 6, 7)
+const area = new FigureArea(5, 7)
+area.SquareArea(5)
+area.CircleArea(7)
+const perimeter = new FigurePerimeter(5, 7, 5)
+perimeter.RectanglePerimeter(5, 7)
+perimeter.TrianglePerimeter(5, 7, 6)
+const type = new Type(5, 6, 7)
+type.TriangleType(5, 6, 7)
